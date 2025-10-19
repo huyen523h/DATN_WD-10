92bce22bb309c408eaf0618c34b18e019d9f4cea
Nguyen Duc Truong <nguyenductruong19092005@gmail.com>
2025-10-19 17:41:04 +0700

    cap nhat staff

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'staff' => \App\Http\Middleware\StaffMiddleware::class,
             'employee' => \App\Http\Middleware\EmployeeMiddleware::class,
             'role' => \App\Http\Middleware\RoleMiddleware::class,
             'permission' => \App\Http\Middleware\PermissionMiddleware::class,
